fb7ef08b5aa8a61244cd16e762aed6a04610c1bd
Vetal <vlyashchenko01@gmail.com>
2023-12-05 11:46:54 +0200

    Test db

--- tests/testutils/create_test_db.go
+++ tests/testutils/create_test_db.go
@@ -22,30 +22,30 @@ func CreateTestDB(cfg Config) *sqlx.DB {
 	db, _ := sqlx.Open("postgres", psqlInfo)
 
 	//Creation a product table
-	//productSchema := `
-	//CREATE TABLE products (
-	//  id SERIAL PRIMARY KEY,
-	//  name TEXT,
-	//  description TEXT
-	//);`
-	//db.MustExec(productSchema)
-	//
+	productSchema := `
+	CREATE TABLE products (
+	 id SERIAL PRIMARY KEY,
+	 name TEXT,
+	 description TEXT
+	);`
+	db.MustExec(productSchema)
+
 	//Creation review table
-	//reviewSchema := `
-	//CREATE TABLE reviews (
-	//  id SERIAL PRIMARY KEY,
-	//  product_id INTEGER,
-	//  user_id INTEGER,
-	//  rating INTEGER,
-	//  content TEXT,
-	//	created_at TIMESTAMP,
-	//	updated_at TIMESTAMP
-	//);`
-	//db.MustExec(reviewSchema)
+	reviewSchema := `
+	CREATE TABLE reviews (
+	 id SERIAL PRIMARY KEY,
+	 product_id INTEGER,
+	 user_id INTEGER,
+	 rating INTEGER,
+	 content TEXT,
+		created_at TIMESTAMP,
+		updated_at TIMESTAMP
+	);`
+	db.MustExec(reviewSchema)
 
 	// Insertion of test prod
-	//db.MustExec(`INSERT INTO products (name, description) VALUES ('Тестовый продукт 1', 'Описание тестового продукта 1')`)
-	//db.MustExec(`INSERT INTO products (name, description) VALUES ('Тестовый продукт 2', 'Описание тестового продукта 2')`)
+	db.MustExec(`INSERT INTO products (name, description) VALUES ('Тестовый продукт 1', 'Описание тестового продукта 1')`)
+	db.MustExec(`INSERT INTO products (name, description) VALUES ('Тестовый продукт 2', 'Описание тестового продукта 2')`)
 
 	return db
 }
