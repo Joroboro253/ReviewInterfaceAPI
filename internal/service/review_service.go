15caa8f6c86edee589ebbac647e145ee40ff8d5d
Vetal <vlyashchenko01@gmail.com>
2023-12-05 10:24:56 +0200

    fixes

--- internal/service/review_service.go
+++ internal/service/review_service.go
@@ -18,17 +18,26 @@ func NewReviewService(db *sqlx.DB) *ReviewService {
 }
 
 func (s *ReviewService) CreateReview(review *models.Review) (int, error) {
-	query, args, err := squirrel.Insert("reviews").
+	builder := squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
+
+	query, args, err := builder.Insert("reviews").
 		Columns("product_id", "user_id", "rating", "content", "created_at", "updated_at").
 		Values(review.ProductID, review.UserID, review.Rating, review.Content, review.CreatedAt, review.UpdatedAt).
 		Suffix("RETURNING id").
 		ToSql()
+	log.Printf("Executing SQL query: %s with args: %v", query, args)
 	if err != nil {
-		return 0, err
+		log.Printf("error building insert SQL query: %v", err)
+		return 0, fmt.Errorf("error building insert SQL query: %w", err)
 	}
 
 	var reviewID int
 	err = s.DB.QueryRow(query, args...).Scan(&reviewID)
+	if err != nil {
+		log.Printf("error executing insert SQL query: %v", err)
+		return 0, fmt.Errorf("error executing insert SQL query: %w", err)
+	}
+
 	return reviewID, nil
 }
 
